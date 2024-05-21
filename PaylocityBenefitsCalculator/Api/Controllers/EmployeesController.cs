b7b9c492728d423afdbb209db493b52c27dbc09d
Jenny <jennifer.eler1@gmail.com>
2024-05-21 14:24:28 -0500

    employee getbyint and getall

--- PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using Api.Dtos.Dependent;
+﻿using System.Text.Json;
+using Api.Dtos.Dependent;
 using Api.Dtos.Employee;
 using Api.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,85 +15,46 @@ public class EmployeesController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<ApiResponse<GetEmployeeDto>>> Get(int id)
     {
-        throw new NotImplementedException();
+        string empText = await System.IO.File.ReadAllTextAsync(@"Dtos/EmployeeData.json");
+        var response = JsonSerializer.Deserialize<List<GetEmployeeDto>>(empText);
+        var result = new ApiResponse<GetEmployeeDto>
+        {
+            Data = null,
+            Success = true
+        };
+         if(response != null) {
+            GetEmployeeDto employee = response.Find(x => x.Id == id);
+            result.Data = employee;
+            if(result.Data == null) {
+                result.Message = $"Can not find employee with id = {id}";
+            }
+        }
+        else {
+            result.Success = false;
+        }
+
+        return result;
     }
 
     [SwaggerOperation(Summary = "Get all employees")]
     [HttpGet("")]
     public async Task<ActionResult<ApiResponse<List<GetEmployeeDto>>>> GetAll()
     {
-        //task: use a more realistic production approach
-        var employees = new List<GetEmployeeDto>
-        {
-            new()
-            {
-                Id = 1,
-                FirstName = "LeBron",
-                LastName = "James",
-                Salary = 75420.99m,
-                DateOfBirth = new DateTime(1984, 12, 30)
-            },
-            new()
-            {
-                Id = 2,
-                FirstName = "Ja",
-                LastName = "Morant",
-                Salary = 92365.22m,
-                DateOfBirth = new DateTime(1999, 8, 10),
-                Dependents = new List<GetDependentDto>
-                {
-                    new()
-                    {
-                        Id = 1,
-                        FirstName = "Spouse",
-                        LastName = "Morant",
-                        Relationship = Relationship.Spouse,
-                        DateOfBirth = new DateTime(1998, 3, 3)
-                    },
-                    new()
-                    {
-                        Id = 2,
-                        FirstName = "Child1",
-                        LastName = "Morant",
-                        Relationship = Relationship.Child,
-                        DateOfBirth = new DateTime(2020, 6, 23)
-                    },
-                    new()
-                    {
-                        Id = 3,
-                        FirstName = "Child2",
-                        LastName = "Morant",
-                        Relationship = Relationship.Child,
-                        DateOfBirth = new DateTime(2021, 5, 18)
-                    }
-                }
-            },
-            new()
-            {
-                Id = 3,
-                FirstName = "Michael",
-                LastName = "Jordan",
-                Salary = 143211.12m,
-                DateOfBirth = new DateTime(1963, 2, 17),
-                Dependents = new List<GetDependentDto>
-                {
-                    new()
-                    {
-                        Id = 4,
-                        FirstName = "DP",
-                        LastName = "Jordan",
-                        Relationship = Relationship.DomesticPartner,
-                        DateOfBirth = new DateTime(1974, 1, 2)
-                    }
-                }
-            }
-        };
 
+        string empText = await System.IO.File.ReadAllTextAsync(@"Dtos/EmployeeData.json");
+        var response = JsonSerializer.Deserialize<List<GetEmployeeDto>>(empText);
         var result = new ApiResponse<List<GetEmployeeDto>>
         {
-            Data = employees,
+            Data = null,
             Success = true
         };
+
+        if (response != null)
+        {
+            result.Data = response;
+        } else {
+            result.Success = false;
+        }
 
         return result;
     }
