ad96e3ee3bce2a371d5b9c45b5a6515868b6549d
Jenny <jennifer.eler1@gmail.com>
2024-05-21 11:49:21 -0500

    get dependent by id

--- PaylocityBenefitsCalculator/Api/Controllers/DependentsController.cs
+++ PaylocityBenefitsCalculator/Api/Controllers/DependentsController.cs
@@ -14,8 +14,30 @@ public class DependentsController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<ApiResponse<GetDependentDto>>> Get(int id)
     {
-        throw new NotImplementedException();
+        // type conflict with API Response
+        string text = await System.IO.File.ReadAllTextAsync(@"Dtos/Dependent/DependentData.json");
+        var response = JsonSerializer.Deserialize<List<GetDependentDto>>(text);
+        var result = new ApiResponse<GetDependentDto>
+        {
+            Data = null,
+            Success = true
+        };
+        if(response != null) {
+            // GetDependentDto dependent = response.Where(x => x.Id == id);
+            GetDependentDto dependent = response.Find(x => x.Id == id);
+            result.Data = dependent;
+            if(result.Data == null) {
+                result.Message = $"Can not find dependent with id = {id}";
+            }
+        }
+        else {
+            result.Success = false;
+        }
+
+        return result;
+
     }
+
 
     [SwaggerOperation(Summary = "Get all dependents")]
     [HttpGet("")]
@@ -23,7 +45,7 @@ public class DependentsController : ControllerBase
     {
         string text = await System.IO.File.ReadAllTextAsync(@"Dtos/Dependent/DependentData.json");
         var response = JsonSerializer.Deserialize<List<GetDependentDto>>(text);
-         var result = new ApiResponse<List<GetDependentDto>>
+        var result = new ApiResponse<List<GetDependentDto>>
         {
             Data = null,
             Success = true
