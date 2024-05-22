ecef15b924f4d7a2922289d4cc9570a8ec24c03c
Jenny <jennifer.eler1@gmail.com>
2024-05-22 16:24:26 -0500

    benefits/calculator class

--- PaylocityBenefitsCalculator/Api/Models/Benefit.cs
+++ PaylocityBenefitsCalculator/Api/Models/Benefit.cs
@@ -1,76 +1,90 @@
+using Api.Dtos.Dependent;
+using Api.Dtos.Employee;
+
 namespace Api.Models;
 
 
-// 26 per year
 public class Benefit {
 
-    public Benefit(decimal inputtedBaseCost = 1000, decimal inputtedAdditionalPartnerCost =  1000, decimal inputtedDependentCost = 600, int inputtedSalarySurcharge = 2, decimal inputtedOlderDepsSurcharge = 200, Employee inputtedEmp) {
-        baseCost = inputtedBaseCost;
-        additionalPartnerCost = inputtedAdditionalPartnerCost;
-        dependentCost = inputtedDependentCost;
-        salarySurcharge = inputtedSalarySurcharge;
-        olderDepsSurcharge = inputtedOlderDepsSurcharge;
-        employee = inputtedEmp;
-
-        TotalCost = CalculateBenefits(inputtedEmp);
+    // I am setting default amounts based on the prompt but the user can also pass in their own values
+    public Benefit(GetEmployeeDto employee, decimal baseCost = 1000, decimal additionalPartnerCost =  1000, decimal dependentCost = 600, int salarySurcharge = 2, decimal olderDepsSurcharge = 200) {
+        _baseCost = baseCost;
+        _additionalPartnerCost = additionalPartnerCost;
+        _dependentCost = dependentCost;
+        _salarySurcharge = salarySurcharge;
+        _olderDepsSurcharge = olderDepsSurcharge;
+        _employee = employee;
+        PaycheckAmountWithDeductions = calculatePaycheckWithDeductions();
+        PaycheckAmount = Math.Round(_employee.Salary/26, 2);
     }
 
-    private static decimal baseCost {get; set;}
+    private decimal _baseCost {get; set;}
 
-    private static decimal additionalPartnerCost {get; set;}
-    private static decimal dependentCost  {get; set;}
-    private static int salarySurcharge {get; set;}
+    private decimal _additionalPartnerCost {get; set;}
+    private decimal _dependentCost  {get; set;}
+    private int _salarySurcharge {get; set;}
 
-    private static decimal olderDepsSurcharge {get; set;}
+    private decimal _olderDepsSurcharge {get; set;}
 
-    private static Employee employee { get; set; }
+    private static GetEmployeeDto _employee { get; set; }
     // per month
-    public decimal TotalCost { get; set; }
+
+    public decimal PaycheckAmountWithDeductions {get; set;}
+
+    public decimal PaycheckAmount {get; set;}
+
+    public decimal OlderDependentCharge {get; set;}
+
+    public decimal SalarySurcharge {get; set;}
 
 
-    // divide by 26  = per paycheck
-
-    // subtract this from salary
-        public static decimal CalculateBenefits(Employee employee) {
+    private decimal calculatePaycheckWithDeductions() {
+        decimal monthlyBenefitCost = calculateMonthlyBenefits();
+        // Salary surcharge not on a monthly basis which is why I separated it from the monthly benefit calculations
+        decimal salarySurcharge = calculateAdditionalSalaryCost(_employee.Salary);
+        // In the future, I would store these number variables instead of putting them here without context
+        decimal paycheckDeductions = ((monthlyBenefitCost * 12) + salarySurcharge)/26;
+        decimal finalPaycheckAmount = (_employee.Salary/26) - paycheckDeductions;
+        // I was waiting until the very end to round 
+        return Math.Round(finalPaycheckAmount, 2);
+    }
+    private decimal calculateMonthlyBenefits() {
 
         // employee added
-        decimal additionalCost = calculateAdditionalSalaryCost(employee.Salary);
-        decimal dependantsCost = calculalateDependentCost(employee.Dependents.ToList());
-        decimal additionalPartnerCost = calculateAdditionalPartnerCost(employee.Dependents.ToList());
-        return baseCost + additionalCost + dependantsCost + additionalPartnerCost;
-     }
+        decimal dependantsCost = calculalateChildDependentCost(_employee.Dependents.ToList());
+        decimal additionalPartnerCost = calculateAdditionalPartnerCost(_employee.Dependents.ToList());
+        decimal olderDepsSurcharge = calculateOlderDeps(_employee.Dependents.ToList());
+        return _baseCost  + dependantsCost  + additionalPartnerCost + olderDepsSurcharge;
+    }
 
-    public static decimal calculalateDependentCost(List<Dependent> dependents) {
+    private decimal calculalateChildDependentCost(List<GetDependentDto> dependents) {
         decimal childrenDepsCost = 0;
-        decimal additionalPartnerCost = 0;
         var childrenDeps = dependents.Where(dep => dep.Relationship == Relationship.Child);
 
         if(childrenDeps.Count() > 0 ) {
-            childrenDepsCost = childrenDeps.Count() * dependentCost;
+            childrenDepsCost = childrenDeps.Count() * _dependentCost;
         }
 
-        return childrenDepsCost + additionalPartnerCost;
+        return Math.Round(childrenDepsCost, 2);
     }
 
-    public static decimal calculateAdditionalPartnerCost(List<Dependent> dependents) {
+    private decimal calculateAdditionalPartnerCost(List<GetDependentDto> dependents) {
+        // I am using .Any instead of .Where here to avoid the issue of including both domestic partner and spouse
         bool hasSpouseDomesticPartner = dependents.Any(dep => dep.Relationship == Relationship.Spouse || dep.Relationship == Relationship.DomesticPartner);
-        return hasSpouseDomesticPartner ? additionalPartnerCost : 0;
+        return hasSpouseDomesticPartner ? _additionalPartnerCost : 0;
     }
     
-    public static decimal calculateAdditionalSalaryCost(decimal salary) {
-        decimal additionalCost = salary * salarySurcharge/ 100;
-        return salary > 80000m ? additionalCost: 0;
+    private decimal calculateAdditionalSalaryCost(decimal salary) {
+        decimal additionalCost = salary * _salarySurcharge/ 100;
+        return salary > 80000 ? additionalCost: 0;
     }
 
     // would rather pass paramater as argument
-    public static decimal calculateOlderDeps(List<Dependent> dependents) {
+    private decimal calculateOlderDeps(List<GetDependentDto> dependents) {
         DateTime today = DateTime.Now;
         DateTime olderPersonAdditionalChargeDate = today.AddYears(-50);
         var olderDeps = dependents.Where(dep => dep.DateOfBirth > olderPersonAdditionalChargeDate);
         int olderDepsAmount = olderDeps.Count();
-        return olderDepsAmount> 0 ? olderDepsSurcharge * olderDepsAmount : 0;
+        return olderDepsAmount> 0 ? _olderDepsSurcharge * olderDepsAmount : 0;
     }
 }
-
-// should i just make these constants???
-// where to put them
